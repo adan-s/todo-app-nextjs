d65c75a4359fc1c6dee7fb17746a7f2a7f4d1f20
Adan Salman <adansalman73@gmail.com>
2024-07-23 16:14:53 +0500

    design changes

--- components/Profile.js
+++ components/Profile.js
@@ -71,7 +71,8 @@ const Profile = () => {
   };
 
   if (!user) {
-    return <p>Loading...</p>;
+    return <p className="text-red-500 mt-96 ml-96 p-2 pt-6 border border-red-500 rounded h-20 items-center justify-center">
+      You are not Logged In. Kindly click on SignOut to redirect on SignIn Page.</p>;
   }
 
   return (
